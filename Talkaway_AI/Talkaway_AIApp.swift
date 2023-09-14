da48401d8b1f52eebff9a3ce9ff42be8b5d1e786
Elsie <elsiehsu1997@gmail.com>
2023-09-13 21:38:07 -0700

    Initial Commit

---
+++ Talkaway_AI/Talkaway_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talkaway_AIApp.swift
+//  Talkaway_AI
+//
+//  Created by Elsie Hsu on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Talkaway_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
