eb5d1731bd6b631af3efac7623956d9d832bc43e
Elsie <elsiehsu1997@gmail.com>
2023-10-31 22:39:38 -0700

    Request for "FeedbackView" debug, for the time bar Kuan-Lin has designed.

---
+++ Talkaway_AI/FeedbackView.swift
@@ -0,0 +1,38 @@
+//
+//  FeedbackView.swift
+//  Talkaway_AI
+//
+//  Created by Elsie Hsu on 10/29/23.
+//
+
+import SwiftUI
+import Foundation
+
+struct FeedbackView: View {
+    //@Binding var progress: Double
+    @State private var progress: Double = 0.0
+    @State private var scaleEffect: CGFloat = 1.0
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            
+            ZStack{
+                VStack {
+                    FeedbackView(progress: $progress)
+                        .onReceive(timer) { _ in
+                            if progress < 1.0 {
+                                progress += 1.0 / (1.0 * 10.0)
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
+                        
+struct FeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackView()
+    }
+}
