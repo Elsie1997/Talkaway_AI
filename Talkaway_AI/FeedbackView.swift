761197778108f5b568e866b6394d89f364610b11
Elsie <elsiehsu1997@gmail.com>
2023-11-23 22:23:11 -0800

    11/23 vers. as of adding feedback page, unfortunately no luck on adding the fraction successfully there...need help dev on this, thanks

--- Talkaway_AI/FeedbackView.swift
+++ Talkaway_AI/FeedbackView.swift
@@ -9,25 +9,54 @@ import SwiftUI
 import Foundation
 
 struct FeedbackView: View {
-    //@Binding var progress: Double
     @State private var progress: Double = 0.0
     @State private var scaleEffect: CGFloat = 1.0
     
     var body: some View {
-        ZStack(alignment: .leading) {
-            let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-            
-            ZStack{
-                VStack {
-                    FeedbackView(progress: $progress)
-                        .onReceive(timer) { _ in
-                            if progress < 1.0 {
-                                progress += 1.0 / (1.0 * 10.0)
+        VStack{
+            ZStack(alignment: .leading) {
+                let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                
+                ZStack{
+                    VStack {
+                        ProgressBarView(progress: $progress)
+                            .onReceive(timer) { _ in
+                                if progress < 1.0 {
+                                    progress += 1.0 / (1.0 * 10.0)
+                                }
                             }
-                        }
+                    }
                 }
             }
-        }
+            
+            //.position(x: 200, y: 50)
+            
+            
+                HStack{
+                    Text("上一句是").font(.system(size: 20))
+                    Image(systemName: "speaker.wave.3.fill")
+                }.position(x: 70, y: 80)
+                
+                HStack{
+                    Text("你剛剛這樣說").font(.system(size: 20)).bold()
+                    Image(systemName: "speaker.wave.3.fill")
+                }.position(x: 90, y: 80)
+                
+                HStack{
+                    Text("需要做一些修正").font(.system(size: 20)).bold()
+                    Image(systemName: "speaker.wave.3.fill")
+                }.position(x: 100, y: 80)
+                
+                HStack{
+                    Text("這樣講更道地").font(.system(size: 20)).bold()
+                    Image(systemName: "speaker.wave.3.fill")
+                }.position(x: 90, y: 80)
+            
+                HStack{
+                    Text("下一個").font(.system(size: 20)).bold().foregroundStyle(.blue)
+                }.position(x: 200, y: 110)
+                
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading).background(Color(hex:0xE4C8DE))
     }
 }
                         
