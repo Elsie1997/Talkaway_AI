052137e1253d27ed907e536f3247d0a55c0da724
ANJU NI <>
2024-04-28 22:34:19 +0800

    [feat] add page 腳本設置頁

--- Talkaway_AI/SettingView.swift
+++ Talkaway_AI/SettingView.swift
@@ -12,64 +12,104 @@ import SwiftUI
 struct SettingView: View {
     var selectedChatScenario: ChatScenario
     
-    @State private var selectedLevel = "初級"
     @State private var selectedGender = "男"
+    @State private var selectedAccent = "美"
+    @State private var selectedSpeed = "0.5"
     @State private var randomContent: String? = nil
+    @State private var isShowAdvanced: Bool = false
     
-    let levels = ["初級", "中級", "高級"]
     let genders = ["男", "女"]
+    let accent = ["美", "英", "澳"]
+    let speed = ["0.5", "0.75", "1", "1.25","1.5"]
     
     var body: some View {
-        VStack{
-            Text("腳本對話設定頁面")
-            // 選擇 難度 && 性別
-            HStack{
-                Text("難度")
-                    .bold(true)
-                    .font(.system(size: 18))
-                
-                Picker(selection: $selectedLevel, label: Text("難度")) {
-                    ForEach(levels, id: \.self) { level in
-                        Text(level).tag(level)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 200)
-                .padding(.leading, 10)
-            }
-            
-            HStack{
-                Text("性別")
-                    .bold(true)
-                    .font(.system(size: 18))
-                
-                Picker(selection: $selectedGender, label: Text("性別")) {
-                    ForEach(genders, id: \.self) { gender in
-                        Text(gender).tag(gender)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 200)
-                .padding(.leading, 10)
-            }
-            
+       
+        
+        ZStack{
             // 顯示情境內容
+         
             Text(randomContent ?? "--")
                 .frame(width: 200)
-                .padding(.top, 50)
                 .foregroundColor(Color(hex: 0x333333))
+                .offset(y: -250)
             
-            // 開始談話按鈕
-            NavigationLink(destination: CheatingView(content: randomContent)) {
-                Text("開始談話")
-                    .font(.headline)
-                    .frame(width: 200, height: 14)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
+            
+
+            VStack{
+                Button("進階設定"){
+                    isShowAdvanced.toggle()
+                }
+                
+                if isShowAdvanced {
+                    // 選擇 性別 & 口音 & 語速
+                    HStack{
+                        Text("性別")
+                            .bold(true)
+                            .font(.system(size: 18))
+                        
+                        Picker(selection: $selectedGender, label: Text("性別")) {
+                            ForEach(genders, id: \.self) { gender in
+                                Text(gender).tag(gender)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .frame(width: 200)
+                        .padding(.leading, 10)
+                        
+                    }.padding(.bottom, 20)
+                    
+                    HStack{
+                        Text("口音")
+                            .bold(true)
+                            .font(.system(size: 18))
+                        
+                        Picker(selection: $selectedAccent, label: Text("難度")) {
+                            ForEach(accent, id: \.self) { level in
+                                Text(level).tag(level)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .frame(width: 200)
+                        .padding(.leading, 10)
+                        
+                    }.padding(.bottom, 20)
+                    
+                    HStack{
+                        Text("語速")
+                            .bold(true)
+                            .font(.system(size: 18))
+                            .padding(.trailing, 10)
+                        
+                        Section{
+                            Picker(selection: $selectedSpeed, label: Text("難度")) {
+                                ForEach(speed, id: \.self) { level in
+                                    Text("X\(level)").tag(level)
+                                }
+                            }
+                            .frame(width: 190)
+                            .padding(.leading, 10)
+                            .background(Color.white)
+                            .cornerRadius(20)
+                        }
+                        
+                    }
+                }
+                
+                
+                
+                
+                // 開始談話按鈕
+                NavigationLink(destination: CheatingView(content: randomContent)) {
+                    Text("開始談話")
+                        .font(.headline)
+                        .frame(width: 200, height: 14)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                }
+                .padding(.top, 50)
             }
-            .padding(.top, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hex:0xE4C8DE))
